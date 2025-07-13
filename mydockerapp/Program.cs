4288022e86cf0dbae52351cf043b2479946c10c3
mohammadzafar <79638116+zafar-dotcom@users.noreply.github.com>
2025-07-13 16:20:44 +0500

    initial commit

---
+++ mydockerapp/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+// ? Only bind to port 80 when in Docker
+if (isDocker)
+{
+   // Add services to the container.
+builder.WebHost.UseUrls("http://*:80");
+}
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+app.UseSwaggerUI();
+//}
+if (isDocker)
+{
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // ?? makes Swagger UI the homepage
+    });
+    app.MapGet("/", () => "Hello from Docker!");
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
