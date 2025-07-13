4504e3f3d2f98b8a5a5dcbe10f09128a04e0406a
mohammadzafar <79638116+zafar-dotcom@users.noreply.github.com>
2025-07-13 20:34:21 +0500

    test ci/cd

--- mydockerapp/Controllers/WeatherForecastController.cs
+++ mydockerapp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace mydockerapp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "treezoinf", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
