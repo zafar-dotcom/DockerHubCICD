584a36692190924d9d1fa74b6fa507bf85e072c9
mohammadzafar <79638116+zafar-dotcom@users.noreply.github.com>
2025-07-14 22:44:41 +0500

    updates endpoint change

--- mydockerapp/Controllers/WeatherForecastController.cs
+++ mydockerapp/Controllers/WeatherForecastController.cs
@@ -19,15 +19,9 @@ namespace mydockerapp.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IActionResult> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(new { status = "Merchant Info Saved" });
         }
     }
 }
